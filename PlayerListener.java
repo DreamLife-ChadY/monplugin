69adf56c8e2ec38842b271f76a2647e3a1144376
theflyer02 <49374645+theflyer02@users.noreply.github.com>
2020-03-31 19:19:21 +0200

    Create PlayerListener.java

---
+++ PlayerListener.java
@@ -0,0 +1,11 @@
+package fr.kalixx.monplugin;
+
+import org.bukkit.event.Listener;
+
+public class PlayerListener implements Listener {
+
+	public PlayerListener(MainPlugin mainPlugin) {
+		
+	}
+
+}
